8f2360420617f8961e13389097de6bc8926d3581
akai-keisanki <iago.ad@outlook.com>
2025-04-17 20:26:42 -0300

    BREAKING (anim.hh & time.hh): moved time-related functions out of anim to time and added `Time` class

--- pers/anim.hh
+++ pers/anim.hh
@@ -7,41 +7,22 @@
 * Basic animation functions *
 ****************************/
 
-#include <thread>
-#include <chrono>
-#include <iostream>
-
 #include "bio.hh"
-#include "vals.hh"
+#include "time.hh"
 
 namespace pers
 {
   namespace anim
   {
 
-    void p_wait (size_t n = 1000, const bool& flush = true)
+    void write (const char* str, const size_t& t = 100)
     {
-      /* Wait N Process Operations */
+      /* Print a string to the terminal with a delay between every character */
 
-      n *= vals.get_p_mult();
-      for (size_t i = 0; i < n ; i ++)
-        if (flush) bio.flush_out();
-    }
-
-    void t_wait (size_t ms = 1000, const bool& flush = true)
-    {
-      /* Wait N Process Operations */
-
-      bio.flush_out();
-      std::this_thread::sleep_for(std::chrono::milliseconds(ms));
-    }
-
-    void write (const char* str, size_t time = 100)
-    {
       for (const char* c = str; *c != '\0'; c ++)
       {
         bio.out(*c);
-        t_wait(time);
+        time.t_wait(t);
       }
     }
 
